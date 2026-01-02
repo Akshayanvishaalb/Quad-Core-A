0afc72a425c6a671299be70a6ff50a5297a5fe55
kushvinth <kushvinth.m@gmail.com>
2026-01-02 23:05:49 +0530

    Remove React plugin from Vite - using vanilla JS with Firebase CDN

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
