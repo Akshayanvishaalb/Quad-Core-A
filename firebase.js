767f86d011b6274f58d8631d1636c4e1c4e9c7c2
AadithiyaaS <aadithiyaas@gmail.com>
2026-01-05 19:54:49 +0530

    Removed dead codes

--- firebase.js
+++ firebase.js
@@ -1,25 +1,3 @@
-// src/firebase.js
-// import admin from "firebase-admin";
-// import fs from "fs";
-// import path from "path";
-// import { fileURLToPath } from "url";
-//
-// // Needed to get __dirname in ES modules
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-//
-// // Read service account JSON manually
-// const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
-// console.log("serviceAccountPath", serviceAccountPath);
-// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
-//
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-//
-// const db = admin.firestore();
-// export default db;
-
 // Below is modified for vercel deployment
 import admin from "firebase-admin";
 
