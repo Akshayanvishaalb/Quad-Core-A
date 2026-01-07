b99cfa256608ecc7f525ee12475ce793158116a8
akshayanvishaalb <akshayanvishaalb@gmail.com>
2026-01-07 19:05:39 +0530

    Fixed env_var name for API_KEY

--- src/Dashboard/dashboard.js
+++ src/Dashboard/dashboard.js
@@ -337,7 +337,7 @@ function loadDoctorsFromGoogleMaps(lat, lng) {
     }
 
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_KEY}&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GoogleMaps_API_Key}&libraries=places`;
     script.async = true;
     script.onload = () => searchDoctors(lat, lng);
     document.body.appendChild(script);
